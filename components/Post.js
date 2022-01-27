357f02163c6c3693d4c2393e96dc1294833588d1
noobcoda <yuemaykl@gmail.com>
2022-01-27 21:43:49 +0000

    like functionality added

--- components/Post.js
+++ components/Post.js
@@ -8,15 +8,18 @@ import {
 } from "@heroicons/react/outline";
 
 import {HeartIcon as HeartIconFilled} from "@heroicons/react/solid";
-import { addDoc, serverTimestamp, collection, onSnapshot, query, orderBy } from "firebase/firestore";
+import { addDoc, serverTimestamp, collection, onSnapshot, query, orderBy, setDoc, doc, deleteDoc } from "firebase/firestore";
 import { useSession } from "next-auth/react";
 import { useEffect, useState } from "react";
 import { db } from "../firebase";
+import Moment from "react-moment";
 
 export default function Post({id,username,userImg,img,caption}){
     const { data: session } = useSession();
     const [comments, setComments] = useState([]);
     const [comment, setComment] = useState("");
+    const [likes, setLikes] = useState([]);
+    const [hasLiked, setHasLiked] = useState(false);
     
     useEffect(
         () => 
@@ -27,8 +30,42 @@ export default function Post({id,username,userImg,img,caption}){
                 ),
                 (snapshot) => setComments(snapshot.docs)
             ),
-        [db],
+        [db,id],
     );
+
+    useEffect(
+        () => 
+            onSnapshot(
+                query(
+                    collection(db,'posts',id,'likes'),
+                ),
+                (snapshot) => setLikes(snapshot.docs)
+            ),
+        [db,id], //anything you use outside of useEffect has to be here, if useEffect depends on it
+    );
+
+    useEffect(
+        () => 
+            setHasLiked(
+                likes.findIndex((like) => like.id === session?.user?.uid) !== -1
+            ),
+        [likes]
+    );
+
+    const likePost = async () => {
+        if (hasLiked) {
+            //delete the document
+            await deleteDoc(doc(db,'posts',id,'likes',session.user.uid));
+        } else {
+            //each like has userid
+            //to make sure it's only one like per user, and
+            //to make sure we can easily search for who liked
+            await setDoc(doc(db,'posts',id,'likes',session.user.uid),{
+                username: session.user.username,
+            })
+        }
+
+    }
 
     const sendComment = async (e) => {
         e.preventDefault(); //prevent page refreshing
@@ -60,7 +97,14 @@ export default function Post({id,username,userImg,img,caption}){
             {/* <Buttons /> */}
             <div className="flex justify-between px-4 pt-4">
                 <div className="flex space-x-4">
-                    <HeartIcon className="btn"/>
+                    {
+                        hasLiked ? (
+                            <HeartIconFilled onClick={likePost} className="btn text-red-500"/>
+                        ) : (
+                            <HeartIcon onClick={likePost} className="btn"/>
+                        )
+                    }
+
                     <ChatIcon className="btn"/>
                     <PaperAirplaneIcon className="btn"/>
                 </div>
@@ -70,6 +114,9 @@ export default function Post({id,username,userImg,img,caption}){
             </div>
             {/* <Caption /> */}
             <p className="p-5 truncate">
+                {likes.length > 0 && (
+                    <p className="font-bold mb-1">{likes.length} likes</p>
+                )}
                 <span className="mr-1"><b>{username}</b> </span>
                 {caption}
             </p>
@@ -80,6 +127,9 @@ export default function Post({id,username,userImg,img,caption}){
                         <div key={comment.id} className="flex items-center space-x-2 mb-3">
                             <img className="h-7 rounded-full" src={comment.data().userImage} alt=""/>
                             <p className="text-sm flex-1"><span className="font-bold">{comment.data().username}</span> {comment.data().comment}</p>
+                            <Moment className="p-2 text-xs" fromNow>
+                                {comment.data().timestamp?.toDate()}
+                            </Moment>
                         </div>
                     ))}
                 </div>
